4bb50668f8bf664da064f90893a39a1d84846de3
Benjamin Anderson II <benjaminalandersonii@gmail.com>
2023-02-25 15:49:04 -0800

    Backup of Studio 2 Part 1 for ENGR103 @ Oregon State University

---
+++ Studio_2_0.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println(" _._     _,-'\"\"`-._");
+  Serial.println("(,-.`._,'(       |\`-/|");
+  Serial.println("    `-.-' \ )-`( , o o)");
+  Serial.println("          `-    \`_`\"'-");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
